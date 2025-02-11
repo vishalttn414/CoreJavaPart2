73ad96e974736973d56da3fa888f249a0889571b
vishalttn414 <vishal.yadav@tothenew.com>
2025-02-11 22:30:58 +0530

    updated Question2.java

--- src/Question2.java
+++ src/Question2.java
@@ -1,20 +1,20 @@
 //Implement multiple inheritance with default method inside interface.
 
 interface Parent1{
-    static void display(){
+    default void display(){
         System.out.println("This is Parent1 method");
     }
 }
 
 interface Parent2{
-    static void display(){
+    default void display(){
         System.out.println("This is Parent2 method");
     }
 }
 class A implements Parent1,Parent2{
     public void display(){
-        Parent1.display();
-        Parent1.display();
+        Parent1.super.display();
+        Parent2.super.display();
         System.out.println("This is class A method");
 
     }
